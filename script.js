9fb8b09f58f85094f0ab7135a05bf76b83743188
BiwashGurung <varungrg10@gmail.com>
2023-11-18 09:40:24 +0545

    added doc query selector

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const searchInput = document.querySelector("#searchInput");
+searchButton = document.querySelector("#searchBtn");
+weatherIcon = document.querySelector(".weather-icon");
+windSpeed = document.querySelector(".wind");
+humidity = document.querySelector(".humidity");
+weather = document.querySelector(".temp");
+city = document.querySelector(".city");
 
 
 
